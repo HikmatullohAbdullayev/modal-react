19c7d904f4be2311a469168f170c521111d50edd
HikmatullohAbdullayev <hikmatullohabdullayev2@gmail.com>
2024-01-22 23:26:46 +0500

    sidebar done

---
+++ src/sidebar/sidebar.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import LogoIcon from "../assets/icon/LogoIcon";
+import HomeIcon from "../assets/icon/HomeIcon";
+import DiscountIcon from "../assets/icon/DiscountIcon";
+import DashboardIcon from "../assets/icon/DashboardIcon";
+import NotificationIcon from "../assets/icon/NotificationIcon";
+import MessaageIcon from "../assets/icon/MessageIcon";
+import SettingIcon from "../assets/icon/SettingIcon";
+import LogOutIcon from "../assets/icon/LogOutIcon";
+
+function Sidebar(props) {
+  return (
+    <>
+      <div className="bg-[#1F1D2B] flex justify-stretch flex-col items-center">
+        <a className="m-6" href="#">
+          <span className="m-4">
+            <LogoIcon />
+          </span>
+        </a>
+        <div className="flex justify-between flex-col items-center gap-9 fill-[#EA7C69">
+          <span
+            className=" bg-transparent fill-[#EA7C69]  border transition-[0.2ms] duration-[ease-in-out] cursor-pointer ml-3 mr-[23px,] mt-[25px] mb-[22px] p-4 rounded-lg border-solid border-[#EA7C69]
+            hover:bg-[#EA7C69] hover:fill-white
+"
+          >
+            <HomeIcon />
+          </span>
+          <span
+            className="  bg-transparent fill-[#EA7C69]  border transition-[0.2ms] duration-[ease-in-out] cursor-pointer ml-3 mr-[23px,] mt-[25px] mb-[22px] p-4 rounded-lg border-solid border-[#EA7C69]
+            hover:bg-[#EA7C69] hover:fill-white
+"
+          >
+            <DiscountIcon />
+          </span>
+          <span
+            className="  bg-transparent fill-[#EA7C69]  border transition-[0.2ms] duration-[ease-in-out] cursor-pointer ml-3 mr-[23px,] mt-[25px] mb-[22px] p-4 rounded-lg border-solid border-[#EA7C69]
+            hover:bg-[#EA7C69] hover:fill-white
+"
+          >
+            <DashboardIcon />
+          </span>
+          <span
+            className="  bg-transparent fill-[#EA7C69]  border transition-[0.2ms] duration-[ease-in-out] cursor-pointer ml-3 mr-[23px,] mt-[25px] mb-[22px] p-4 rounded-lg border-solid border-[#EA7C69]
+            hover:bg-[#EA7C69] hover:fill-white
+"
+          >
+            <MessaageIcon />
+          </span>
+          <span
+            className=" bg-transparent fill-[#EA7C69]  border transition-[0.2ms] duration-[ease-in-out] cursor-pointer ml-3 mr-[23px,] mt-[25px] mb-[22px] p-4 rounded-lg border-solid border-[#EA7C69]
+            hover:bg-[#EA7C69] hover:fill-white
+"
+          >
+            <NotificationIcon />
+          </span>
+          <span
+            className="  bg-transparent fill-[#EA7C69]  border transition-[0.2ms] duration-[ease-in-out] cursor-pointer ml-3 mr-[23px,] mt-[25px] mb-[22px] p-4 rounded-lg border-solid border-[#EA7C69]
+            hover:bg-[#EA7C69] hover:fill-white
+"
+          >
+            <SettingIcon />
+          </span>
+        </div>
+        <button
+          className="  bg-transparent fill-[#EA7C69]  border transition-[0.2ms] duration-[ease-in-out] cursor-pointer ml-3 mr-[23px,] mt-[25px] mb-[22px] p-4 rounded-lg border-solid border-[#EA7C69]
+             hover:bg-[#EA7C69] hover:fill-white
+ "
+        >
+          <LogOutIcon />
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default Sidebar;
